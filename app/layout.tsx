f3324ffa38d0c5d8c8906184fdbb4a8b1b3fea16
Chris <medialabproyectos@gmail.com>
2026-06-17 15:26:51 -0500

    co

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,6 +119,17 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End Google Tag Manager */}
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MZ5PFVTK');`
+          }}
+        />
+        {/* End Google Tag Manager */}
         {/* ContentSquare / Hotjar */}
         <script src="https://t.contentsquare.net/uxa/1a3415ef76f8a.js" async />
         {/* End ContentSquare / Hotjar */}
@@ -128,6 +139,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-N2XD595J"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MZ5PFVTK"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
